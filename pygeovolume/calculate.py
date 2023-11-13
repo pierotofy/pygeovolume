24ecb7548ac4e4ff5bd69151430dc7573b537917
Piero Toffanin <pt@masseranolabs.com>
2023-11-13 18:22:28 -0500

    Works

--- pygeovolume/calculate.py
+++ pygeovolume/calculate.py
@@ -1,50 +1,83 @@
 import os
 import rasterio
+import rasterio.mask
 from pyproj import CRS, Transformer
+from scipy.interpolate import griddata
+import numpy as np
+
 from .geojson import read_polygon
 
-def calc_volume(input_dem, pts=None, pts_epsg=None, geojson_polygon=None):
+def calc_volume(input_dem, pts=None, pts_epsg=None, geojson_polygon=None,
+                interp_method='cubic', decimals=4):
     if not os.path.isfile(input_dem):
-        raise IOError(f"{input_dem} does not exist")
+        raise ValueError(f"{input_dem} does not exist")
 
     crs = None
     with rasterio.open(input_dem) as d:
         if d.crs is None:
-            raise IOError(f"{input_dem} does not have a CRS")
+            raise ValueError(f"{input_dem} does not have a CRS")
         crs = CRS.from_epsg(d.crs.to_epsg())
     
     if pts is None and pts_epsg is None and geojson_polygon is not None:
         # Read GeoJSON points
         pts = read_polygon(geojson_polygon)
-        
-        # Convert to DEM crs
-        transformer = Transformer.from_crs(
-            CRS.from_epsg(4326),
-            crs
-        )
+        return calc_volume(input_dem, pts=pts, pts_epsg=4326)
+    
+    # Convert to DEM crs
+    transformer = Transformer.from_crs(
+        CRS.from_epsg(pts_epsg),
+        crs
+    )
+    dem_pts = [transformer.transform(p[1], p[0]) for p in pts]
+    
+    # Some checks
+    if len(dem_pts) < 2:
+        raise ValueError("Insufficient points to form a polygon")
 
-        trans_pts = [transformer.transform(p[1], p[0]) for p in pts]
-        return calc_volume(input_dem, trans_pts, 4326)
+    # Close loop if needed
+    if not np.array_equal(dem_pts[0], dem_pts[-1]):
+        dem_pts.append(dem_pts[0])
+    
+    dem_pts = np.array(dem_pts)
 
-    buffer = 0
+    polygon = {"coordinates": [dem_pts], "type": "Polygon"}
+
+    # Remove last point (loop close)
+    dem_pts = dem_pts[:-1]
+    
     with rasterio.open(input_dem) as d:
-        # Convert input points to pixel coordinates
-        pixel_coords = [d.index(*p) for p in pts]
+        px_w = d.transform[0]
+        px_h = d.transform[4]
+
+        # Area of a pixel in square units
+        px_area = abs(px_w * px_h)
+
+        rast_dem, transform = rasterio.mask.mask(d, [polygon], crop=True, all_touched=True, indexes=1, nodata=np.nan)
+        h, w = rast_dem.shape
+
+        # X/Y coordinates in transform coordinates
+        ys, xs = np.array(rasterio.transform.rowcol(transform, dem_pts[:,0], dem_pts[:,1]))
+        zs = rast_dem[ys,xs]
+
+        if np.any(xs<0) or np.any(xs>=w) or np.any(ys<0) or np.any(ys>=h):
+            raise ValueError("Points are out of bounds")
         
-        # Determine the window bounds
-        min_y = max(0, min(coord[0] for coord in pixel_coords) - buffer)
-        min_x = max(0, min(coord[1] for coord in pixel_coords) - buffer)
-        max_y = min(d.width, max(coord[0] for coord in pixel_coords) + buffer)
-        max_x = min(d.height, max(coord[1] for coord in pixel_coords) + buffer)
+        # Create a grid for interpolation
+        x_grid, y_grid = np.meshgrid(np.linspace(0, w - 1, w), np.linspace(0, h - 1, h))
 
-        w = rasterio.windows.Window.from_slices((min_y, max_y), (min_x, max_x))
-        transform = d.window_transform(w)
+        # Perform spline interpolation using griddata
+        base = np.round(griddata(np.column_stack((xs, ys)), zs, (x_grid, y_grid), method=interp_method), decimals=9)
 
-        rast = d.read(1, window=w)
+        # Calculate volume
+        diff = rast_dem - base
+        volume = np.nansum(diff) * px_area
 
-        print(min_x, min_y, max_x, max_y)
-        print(rast)
+        return np.round(volume, decimals=decimals)
 
-        with rasterio.open(input_dem + "out.tif", "w", width=w.width, height=w.height, dtype=rast.dtype, count=1, transform=transform) as out:
-            out.write(rast, 1)
-    return "OK"
+        # import matplotlib.pyplot as plt
+        # fig, ax = plt.subplots()
+        # ax.imshow(rast_dem)
+        # plt.scatter(xs, ys, c=zs, cmap='viridis', s=50, edgecolors='k')
+        # plt.colorbar(label='Z values')
+        # plt.title('Debug')
+        # plt.show()
